c792e58c55ab9f4983d6e90d0cec3ba2e81e9986
Syeda-Jaweria-Amjad <syedjaweria2001@gmail.com>
2024-08-29 18:13:18 +0500

    first commit

---
+++ src/comps/CompFrame.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ChatbotIframe = () => {
+  return (
+    <div>
+      <div style={{ height: '100vh', width: '40%' , margin:"auto" }}>
+
+      <iframe
+        src="https://www.chatbase.co/chatbot-iframe/UZ6LBHuHZLOTZrLUCz4hD"
+        width="100%"
+        style={{ height: '100%', minHeight: '700px' }}
+        frameBorder="0"
+      ></iframe>
+      
+    </div>
+    </div>
+  )
+}
+
+export default ChatbotIframe
